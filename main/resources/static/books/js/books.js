c4a7285bbb7e02ab878176a367c6a0945fca724a
C. Wyatt Cook <c.w.cook@live.com>
2018-04-07 22:50:50 -0500

    Changed the appearance of many items on multiple pages

--- main/resources/static/books/js/books.js
+++ main/resources/static/books/js/books.js
@@ -1,49 +1,26 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content 
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+ // Get the modal
+ var modal = document.getElementById('myModal');
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches(".dropbtn")) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-};*/
-$('.dropdown-menu a').click(function(){
-    $('#selectedCondition').text($(this).text());
-  });
-  $('.dropdown-menu a').click(function(){
-    $('#selectedPrice').text($(this).text());
-  });
+ // Get the button that opens the modal
+ var btn = document.getElementById("myBtn");
 
-  function myFunction() {
-    var x = document.getElementById("filterSelections");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
+ // Get the <span> element that closes the modal
+ var span = document.getElementsByClassName("close")[0];
 
-var acc = document.getElementsByClassName("accordion");
-var i;
+ // When the user clicks on the button, open the modal 
+ btn.onclick = function () {
+     modal.style.display = "block";
+ }
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
+ // When the user clicks on <span> (x), close the modal
+ span.onclick = function () {
+     modal.style.display = "none";
+ }
+
+ // When the user clicks anywhere outside of the modal, close it
+ window.onclick = function (event) {
+     if (event.target == modal) {
+         modal.style.display = "none";
+     }
+ }
+
