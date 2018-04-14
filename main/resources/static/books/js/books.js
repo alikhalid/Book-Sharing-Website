ed30250da802516a07df3b90da89d8f0ab2561c5
C. Wyatt Cook <c.w.cook@live.com>
2018-04-14 17:44:04 -0500

    Multiple Fixes

--- main/resources/static/books/js/books.js
+++ main/resources/static/books/js/books.js
@@ -1,26 +1,23 @@
- // Get the modal
- var modal = document.getElementById('myModal');
 
- // Get the button that opens the modal
- var btn = document.getElementById("myBtn");
 
- // Get the <span> element that closes the modal
- var span = document.getElementsByClassName("close")[0];
-
- // When the user clicks on the button, open the modal 
- btn.onclick = function () {
-     modal.style.display = "block";
+ var acc = document.getElementsByClassName("accordion");
+ var i;
+ 
+ for (i = 0; i < acc.length; i++) {
+   acc[i].addEventListener("click", function() {
+     this.classList.toggle("active");
+     var panel = this.nextElementSibling;
+     if (panel.style.maxHeight){
+       panel.style.maxHeight = null;
+     } else {
+       panel.style.maxHeight = panel.scrollHeight + "px";
+     } 
+   });
  }
 
- // When the user clicks on <span> (x), close the modal
- span.onclick = function () {
-     modal.style.display = "none";
- }
-
- // When the user clicks anywhere outside of the modal, close it
- window.onclick = function (event) {
-     if (event.target == modal) {
-         modal.style.display = "none";
-     }
- }
-
+function openLeftMenu() {
+    document.getElementById("leftMenu").style.display = "block";
+}
+function closeLeftMenu() {
+    document.getElementById("leftMenu").style.display = "none";
+}
